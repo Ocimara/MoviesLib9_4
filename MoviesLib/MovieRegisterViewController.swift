485eb8cc172600d189f2bb03e8c0ff577f99cf79
Usuário Convidado <Guest@pa06micro24.local>
2018-04-09 20:04:33 -0300

    parte 01

---
+++ MoviesLib/MovieRegisterViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MovieRegisterViewController.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 09/04/18.
+//  Copyright © 2018 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+class MovieRegisterViewController: UIViewController {
+
+    
+    @IBOutlet weak var tfTitle: UITextField!
+    @IBOutlet weak var lbCategories: UILabel!
+    @IBOutlet weak var tfRating: UITextField!
+    @IBOutlet weak var tfDuration: UITextField!
+    @IBOutlet weak var ivPoster: UIImageView!
+    @IBOutlet weak var tvSummary: UITextView!
+    @IBOutlet weak var btAddUpdate: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
